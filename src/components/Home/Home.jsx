a864261f8465251e967ca4bae1880300329d6182
Sazedur <sazedur11ab@gmail.com>
2025-07-28 22:14:07 +0600

    form setup by useState

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,28 +1,58 @@
-
+import { useState } from "react";
 
 const Home = () => {
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
 
-    const handleSubmit = e =>{
-        e.preventDefault();
-        console.log(e.target.name.value);
-        console.log(e.target.email.value);
-        console.log(e.target.phone.value)
-        console.log('submited')
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+    console.log(email);
+    console.log(password)
+  };
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit} className="bg-gray-400 w-1/2 mx-auto mt-20 p-10">
-                <input className="border-2 border-gray-300 mb-2 p-1" type="text" name="name" />
-                <br />
-                <input className="border-2 border-gray-300 mb-2 p-1" type="email" name="email" id="" />
-                <br />
-                <input className="border-2 border-gray-300 mb-2 p-1" type="number" name="phone" id="" />
-                <br />
-                <input className="btn btn-primary" type="submit" value="Submit" />
-            </form>
-        </div>
-    );
+  const handleOnChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const handleOnChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleOnChangePassword = (e) => {
+    setPassword(e.target.value)
+  };
+
+  return (
+    <div className="flex justify-center  bg-gray-500 w-1/3 mx-auto mt-20 p-4">
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleOnChangeName}
+          className="border-2 border-gray-300 p-1 my-2"
+          type="text"
+          name="name"
+          id=""
+        />
+        <br />
+        <input
+          onChange={handleOnChangeEmail}
+          className="border-2 border-gray-300 p-1 my-2"
+          type="email"
+          name="email"
+          id=""
+        />
+        <br />
+        <input
+            onChange={handleOnChangePassword}
+          className="border-2 border-gray-300 p-1 my-2"
+          type="password"
+          name="password"
+          id=""
+        />
+        <br />
+        <input className="btn btn-primary" type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
